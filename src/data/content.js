6c5ebc44544fd9b5d327878473e7b6dca2161c1f
Romina <rominaggargano@gmail.com>
2026-04-09 13:02:50 -0300

    feat: estructura inical y archivos base

---
+++ src/data/content.js
@@ -0,0 +1,40 @@
+export const translations = {
+    es: {
+        contacto: {
+            title: "¡Hola!",
+            intro: "Podes dejarme un mensaje o contactarme por mis redes.",
+            body: ""
+        },
+        curriculum: {
+            title: "Un poco sobre mi camino...",
+            intro: `Siempre tuve esa chispa de querer saber cómo funcionan las cosas.  
+            Mi recorrido empezó con la curiosidad por los datos en la UNSAM,
+            donde descubrí que la Inteligencia Artificial no es magia, sino lógica y estadística aplicada.`,
+            body: `Hoy, mientras avanzo en mi formación en el IFTS N°29 y en la UNDEC, disfruto transformando 
+            ideas en código real. Me apasiona el desarrollo y me divierte enfrentar esos 'agujeros de conejo' que son los 
+            bugs difíciles de resolver.
+            Mi enfoque es simple: aprender haciendo, escribir código limpio y nunca dejar de preguntarme '¿Habrá una forma mejor de hacer esto?'.`,
+            skillsTitle: ['Herramientas & Tecnologías'],
+            skills: ['Python', 'SQL', 'Astro','GitHub'],
+            downloadBtn : 'Descargar CV (PDF)'
+
+        }
+    },
+    en: {
+        contacto: {
+            title: "Hello!",
+            intro: "You can leave me a message or contact me through my social networks.",
+            body: ""
+        },
+        curriculum: {
+            title: 'A bit about my journey...',
+            intro: `I've always had that spark of wanting to know how things work.
+            My journey began with a curiosity for data at UNSAM, where I discoverd that AI is not magic,
+            but logic and applied statistics.`,
+            body: `Today, while progressing through my studies at IFTS N°29 and at UNDEC, I enjoy transforming ideas into real code.
+            I'm passionate about development and I find rewarding to tackle those technical challenges that requires persistence to be solved`,
+            skillsTitle: ['Python', 'SQL', 'Astro','GitHub'],
+            downloadBtn: 'Download CV (PDF)'
+        }
+    }
+};
